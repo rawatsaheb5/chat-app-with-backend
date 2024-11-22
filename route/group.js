fd8cb33d4719c221e5115dc5e14eefecc47e8368
rawatsaheb5 <diamondcut2004@gmail.com>
2024-11-22 16:48:02 +0530

    added add-to-group route

--- route/group.js
+++ route/group.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const verifyToken = require('../middleware/auth');
-const { createGroup, MakeOtherUserAsAdmin, removeUserAsAdmin } = require('../controller/group');
+const { createGroup, MakeOtherUserAsAdmin, removeUserAsAdmin, addMemberToTheGroup } = require('../controller/group');
 
 const router = express.Router();
 
@@ -8,4 +8,5 @@ const router = express.Router();
 router.post('/', verifyToken, createGroup)
 router.put('/make-admin', verifyToken, MakeOtherUserAsAdmin)
 router.put('/remove-from-admin', verifyToken, removeUserAsAdmin)
+router.put('/add-to-group', verifyToken, addMemberToTheGroup)
 module.exports = router;
